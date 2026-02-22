9fb88e24b0972af76396fb924e097e91e0da4d06
x-krizn <x.krizn@gmail.com>
2026-02-21 19:19:04 -0500

    Update main.tsx
    pwa redo

--- src/main.tsx
+++ src/main.tsx
@@ -7,4 +7,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-);
+)
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  immediate: true,
+});
